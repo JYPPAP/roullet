8d683aa7ce0badc0f4b64de8368a726795f5f85a
JOSCOM\yungs <yungsu222@naver.com>
2021-10-01 22:01:14 +0900

    Javascript 1. 중복 클릭 방지용 코드 작성. 2. 룰렛 애니메이션 함수 작성.

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 21-10-01 추가 및 수정사항
+
+### 해야 할 일
+1. 팝업창 만들기
+  > 꽝 팝업, 당첨 팝업, 응모권 부족 팝업.
+  
 ### HTML 
 1. HEAD에 script 불러올 때 defer 속성 추가함.
 2. #ticket_count_num의 자식요소였던 p태그를 형제요소로 변경함.
@@ -8,6 +13,7 @@
 1. .ticket_count의 position을 relative에서 absolute로 변경.
 2. p 태그가 #ticket_count_num의 자식요소일 때 작성했던 것을 정상위치(.ticket_count)로 옮김.
 3. 오타 수정 및 img 에 걸려있던 cursor: pointer를 부모요소로 옮겨줌.
+4. item_image 출력했을 때 위치 세부 조정. ( 2px 이동)
 ===============================
 ### Javascript
 1. 티켓의 갯수, 스타트버튼, 티켓 변동내역을 변수 처리함.
@@ -16,6 +22,19 @@
 4. 스타트버튼에 이벤트를 걸어서 ticketCheck에서 ticketCount의 값이 0보다 클 때 true, 아니면 None를 반환하도록 설정
 5. ticketCheck의 값이 false일 때 ticketCount를 0으로 하는 값을 반환하고 종료.
 6. ticketCheck의 값이 True일 때 ticketCount값을 1 빼고 그 값을 #ticket_count_num에 입력 후 난수생성 및 상품번호 생성하고 ticketCount와 상단에서 선언 할 rouletteAct 함수를 호출.
+7. 난수, 상품번호, 클릭 플래그를 변수 처리함.
+8. 중복클릭 방지용 코드 완성
+    - clickFlag값의 초기값이 false로 저장되어 있고 초기값이 clickCheck 함수를 실행하면 true로 변경된다.
+    그 다음 중복클릭을 했을 때 clickFlag에는 true 값이 저장되어 있으며 그 true값은 return clickFlag로 true가 반환된다.
+    if문에서 반환된 값이 참이고, return이 있다면 끝나게 된다.
+    이렇게 중복을 막았고, 응모권이 없을때는 경고창을 출력할 것이고,
+    재실행이 필요한 응모권이 있을 때. 그 곳에 마지막 실행 후 1초가 지났을 때 clickFlag의 값을 false로 초기화 할 수 있도록 작성했다.
+9. 룰렛 애니메이션 함수 작성.
+    - for 구문을 이용해서 0부터 난수까지 1씩 증가
+    setTimeout을 이용해서 i * 100의 값 동안 opacity = 1로 보이게 설정
+    (i * 100) + 200의 시간동안 opacity = 0으로 안보이게 설정
+    마지막 이미지 앞에서 끝나기 때문에 (randomNum * 100) + 250의 시간일 때 opacity = 1로 지정해서 보일 수 있도록 변경.
+    중복클릭 초기화를 위해서 끝날 때 clickFlag를 false로 초기화 할 수 있도록 작성.
 ===============================
 ### HTML
 
--- js/common.js
+++ js/common.js
@@ -1,21 +1,68 @@
 document.addEventListener("DOMContentLoaded", () => {
   let ticketCount = 10;
+  let randomNum;
+  let itemNum;
+  let clickFlag = false;
   const startBtn = document.querySelector('.start_roulette');
   const ticketChange = document.querySelector('.ticket_change');
   const itemRoulette = document.getElementsByClassName('item_image');
-  /* test용 출력 */
-  // console.log(startBtn);
-  // console.log(ticketChange);
-  console.log(itemRoulette);
-  console.log(itemRoulette.length);
 
-  const rouletteAct = function () {
+  function noMoreTicket(ticketCount) {
+    console.log("-----------------------------");
+    console.log("응모권이 부족!할 때 출력될 로그");
+    console.log("Ticket 이 없습니다.");
+    console.log("현재 티켓의 갯수: " + ticketCount);
+  }
+
+  function rouletteAct(randomNum, itemNum) {
     /* 이 안에 룰렛관련 동작을 넣고 아래에서 호출. */
-    
+    console.log("-----------------------------");
+    console.log("룰렛을 돌릴 때 출력될 로그");
+    console.log("난수: " + randomNum);
+    console.log("상품 번호: " + itemNum);
+
+    for ( let i=0; i<randomNum; i++ ) {
+      /* 이미지 보이게 */
+      setTimeout(() => {
+        itemRoulette[(i % 14)].style.opacity = "1";
+      }, i*100);
+      /* 이미지 안보이게 */
+      setTimeout(() => {
+        itemRoulette[(i % 14)].style.opacity = "0";
+      }, (i*100) + 200);
+    }
+    /* 마지막 이미지 출력 */
+    setTimeout(() => {
+      itemRoulette[(itemNum)].style.opacity = "1";
+    }, (randomNum*100) + 250);
+
+    /* 중복클릭 초기화 */
+    setTimeout(() => {
+      clickFlag = false;
+    }, (randomNum*100) + 1250);
   }
 
   /* 티켓 갯수를 HTML에 출력하기 위한 코드 */
   document.getElementById('ticket_count_num').innerHTML = ticketCount;
+
+  /* 중복클릭 차단 */
+  /*
+  초기값이 false로 저장되어 있고 
+    초기값이 clickCheck 함수를 실행하면 true로 변경된다.
+    그 다음 중복클릭을 했을 때 clickFlag에는 true 값이 저장되어 있으며 그 true값은 return clickFlag로 true가 반환된다.
+    if문에서 반환된 값이 참이고, return이 있다면 끝나게 된다.
+    이렇게 중복을 막았고,
+    응모권이 없을때는 경고창을 출력할 것이고,
+    재실행이 필요한 응모권이 있을 때. 그 곳에 마지막 실행 후 1초가 지났을 때 clickFlag의 값을 false로 초기화 할 수 있도록 작성했다.
+  */
+  function clickCheck(){
+    if(clickFlag) {
+      return clickFlag;
+    } else {
+      clickFlag = true;
+      return false;
+    }
+  }
 
   /* START 버튼에 클릭이벤트를 넣고 응모권 갯수 비교하고 결과값 리턴.
   룰렛 애니메이션?을 실행할 수 있는 함수를 리턴.
@@ -23,37 +70,37 @@ document.addEventListener("DOMContentLoaded", () => {
   startBtn.onclick = e => {
     console.log("=============================");
     console.log("StartBtn is Click");
+
+    /* 중복실행 방지용 코드 */
+    /* 값이 참이면 여기서 끝난다.
+    값이 거짓이면 아래 코드로 넘어간다. */
+    if(clickCheck()){return;}
+
+    /* 뽑기 후 초기화. */
+    for ( let i=0; i<itemRoulette.length; i++ ) {
+      itemRoulette[(i % 14)].style.opacity = "0";
+    }
     // console.log(ticketCount);
+    /* 티켓유무 판단 */
     let ticketCheck = ticketCount > 0 ? true : false;
 
-    /* 여기에서 0일경우 미리 걸러버리기. */
+    /* 티켓이 없을 경우 */
     if(!ticketCheck) {
-      /* 여기에서 응모권이 부족합니다 팝업 띄우기용 리턴 */
-      console.log("응모권이 부족할 때 출력될 로그");
-      console.log("=============================");
-      return ticketCount = 0;
+      return clickFlag, noMoreTicket(ticketCount);
     }
-    /* 여기에서 티켓이 1개 이상 있는 경우. */
-    if(ticketCheck) {
-      ticketCount--;
-      /* 티켓 갯수를 HTML에 출력하기 위한 코드 */
-      document.getElementById('ticket_count_num').innerHTML = ticketCount;
-      // console.log("ticketCheck: " + ticketCheck);
-      // console.log("ticketCount: " + ticketCount);
 
-      /* 티켓 1개 뺐으니까 난수 생성. */
+    /* 티켓이 1개 이상 있는 경우. */
+    if(ticketCheck) {
+      /* 티켓 갯수를 줄이고 출력 */
+      ticketCount--;
+      document.getElementById('ticket_count_num').innerHTML = ticketCount;
+      /* 난수와 아이템의 인덱스값 생성. */
+      // 여러 1바퀴 좀 넘게 돌아야 보는맛이 있기 때문에 최소값 24로 지정 2바퀴 넘게돌면 지루할 수 있으니 최대값 38로 지정.
       const randomNum = Math.floor(Math.random() * ( 38 - 24)) + 24;
       const itemNum = (randomNum % 14);
-      // console.log("난수: " + randomNum);
-      // console.log("상품번호: " + itemNum);
 
-      /* 여기에서 ticketCount의 값과 룰렛돌리기용 함수를 리턴 */
-      /* 티켓 갯수를 HTML에 출력하기 위한 코드 */
-      console.log("룰렛을 돌릴 때 출력될 로그");
-      console.log("=============================");
-      return ticketCount, rouletteAct;
+      /* 티켓의 총 갯수와 룰렛돌리기 함수를 리턴 */
+      return clickFlag, ticketCount, rouletteAct(randomNum, itemNum);
     }
   }
-
-
 });
