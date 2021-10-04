f811fdd286930d6db47880a1b9e4a3171d6fd12a
JYPPAP <yungsu222@naver.com>
2021-10-04 10:40:27 +0900

    SCSS 1. ok와 sorry를 바꿈  - sorry가 기본값으로 되어있었는데, 그것을 ok로 변경함. Javascript 1. popup.js에서 DOM이 로딩된 이후 setTimeout을 0.15초 걸어줘서 룰렛창에서 가져온 값을 읽어들이는데 충분한 시간을 줌. 2. CSS에서 기본값을 sorry에서 ok로 변경했기 때문에 그 부분에 해당하는 내용 또한 바꿔줌. 3. 텍스트 값을 가져오는 변수를 let 에서 const로 변경함.

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let textCheck = document.getElementById('ok_text').innerText;
-  
-  if (textCheck) {
-    document.querySelector('.content_image').classList.add('ok');
-  }
+  setTimeout(() => {
+    const textCheck = document.getElementById('ok_text').innerText;
+    
+    if (!textCheck) {
+      document.querySelector('.content_image').classList.add('sorry');
+    }
+  }, 150);
   closeBtn = document.querySelector('.end_image');
   closeBtn.onclick = e => {
     window.close();
